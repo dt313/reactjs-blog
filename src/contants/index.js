262a96a4c5283a2d16ceeecbf3b301d59f280681
dt313 <dt313.dev21@gmail.com>
2025-03-03 19:58:19 +0900

    change api url

--- src/contants/index.js
+++ src/contants/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8080/oauth2/authorization';
+const API_URL = `${process.env.REACT_APP_API_URL_BASE}/oauth2/authorization`;
 
 export const GOOGLE_OAUTH_URL = API_URL + '/google?to=/login';
 export const GITHUB_OAUTH_URL = API_URL + '/github';
